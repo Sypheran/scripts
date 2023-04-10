aa3600eefb86538453a8434e2e49cc9d7339fe2d
Sypheran <104648429+Sypheran@users.noreply.github.com>
2023-04-10 17:02:35 -0400

    Create BigFootSlider.lua

---
+++ BigFootSlider.lua
@@ -0,0 +1,5 @@
+game.Players.LocalPlayer.Character["Left Leg"]:AddPropertyEmulator("Size")
+wait(0.5)
+game.Players.LocalPlayer.Character["Left Leg"].Massless = true
+game.Players.LocalPlayer.Character["Left Leg"].Size = Vector3.new (12.3, 2.11, 10.2)
+game.Players.LocalPlayer.Character["Left Leg"].Transparency = 0.9     
